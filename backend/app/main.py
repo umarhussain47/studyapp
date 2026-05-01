f13a15b0fd58e100c014291c421d7b0856f1d87b
umarhussain47 <umar47hussain@hotmail.com>
2026-05-01 14:57:14 +0500

    Initial commit

--- backend/app/main.py
+++ backend/app/main.py
@@ -8,7 +8,10 @@ app = FastAPI()
 # Tell FastAPI to trust requests coming from your React frontend
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"], 
+    allow_origins=[
+    "https://your-frontend-name.vercel.app", 
+    "http://localhost:5173"
+    ], 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
